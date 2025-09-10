5bbf992c064ece98293f60a0adcc9ab7c6c9498a
Michael Doughty <yekim.codes@gmail.com>
2025-09-10 13:12:22 -0400

    Initial commit - All relative files added in this first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+// src/Header.js
+import React from 'react';
+import logo from '../assets/my_logo_cropped_mikedev.png';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+          <nav>
+            <div className="logo">
+                <span className="glaze-img">
+                    <img src={logo} className="logo-img" alt="Logo" />
+                </span>
+                <span className="text-2x1 font-bold">Mike<span className="dev glaze gradient-text">Dev</span>
+                </span>
+            </div>
+            <ul>
+              <li><a className="glaze gradient-text" href="/">Home</a></li>
+              <li><a className="glaze gradient-text" href="/about">About me</a></li>
+              <li><a className="glaze gradient-text" href="/contact">Contact</a></li>
+            </ul>
+          </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <main>
+      <div className="container">
+          <h2>Welcome to My Portfolio</h2>
+          <p>This is a brief introduction about myself and my work.</p>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
