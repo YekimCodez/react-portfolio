cc8b7e471768f67365a3f56097a4a339bf8fef27
Michael Doughty <yekim.codes@gmail.com>
2025-09-12 05:45:45 -0400

    - Added background image to main section - Created reusable Button component - Updated styles in App.css for better layout and design - Adjusted Main.jsx to include new class for main section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import Button from './Button';
+// import profile from '../assets/portfolio-pic.png';
 
 function Main() {
   return (
-    <main>
+    <main className="main-section">
       <div className="container">
-          <h2>Welcome to My Portfolio</h2>
-          <p>This is a brief introduction about myself and my work.</p>
+          <section className="inside-container">
+            {/* <img src={profile} alt="Profile" className="profile-image" /> */}
+            <h2 className="welcome-heading">Michael Doughty</h2>
+            <h3 className="title">Web Developer</h3>
+            <p className="bio">
+              Building clean, efficient, and user-focused web solutions for small and medium businesses and individual entrepreneurs who want to strengthen their digital presence and connect with their audience online.
+            </p>
+            <Button />
+          </section>
       </div>
     </main>
   );
